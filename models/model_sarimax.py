c0319462ae910e57bf2438995bf1535d80dfd2d0
Sudishma Karki <sudishma.karki@city.ac.uk>
2025-04-10 02:09:11 +0100

    Inital start for the SARIMAX

---
+++ models/model_sarimax.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import os
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import TimeSeriesSplit
+import logging
+logging.basicConfig(level=logging.WARNING)
+logging.disable(logging.INFO)
+from prophet import Prophet
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.metrics import mean_absolute_percentage_error
+import itertools
+from itertools import product
+import holidays
+from prophet.diagnostics import cross_validation, performance_metrics
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+import scipy.stats as stats
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.stats.diagnostic import acorr_ljungbox
+
+                         ##BASELINE MODEL##
